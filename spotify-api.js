c58f6984803c945f0051038599bbe38a3894525f
iarigby <marvinzem@gmail.com>
2019-09-02 03:38:04 +0400

    ew

--- spotify-api.js
+++ spotify-api.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 function get_playlist_id(song_name) {
     return new Promise()
 }
@@ -12,7 +14,30 @@ function get_playlist_contents(playlist_id) {
     })
 }
 
+const SpotifyWebApi = require('spotify-web-api-node')
+const spotify_api = new SpotifyWebApi({
+    clientid: '4561e903b7db4f5ea09b30842bfa3f67',
+    clientSecret: process.env.api_secret,
+    redirectUri: 'http://localhost'
+})
+spotify_api.setAccessToken(process.env.access_token)
+// mine is
+// 21aamjqllbwgchskywbugsopa
+// liked songs playlist
+// 7kGIhqEo5HGYTlFFeIi1j7
+function get_playlists(user_id) {
+    spotify_api.getUserPlaylists(user_id).then(
+        function(data) {
+            console.log(data.body)
+        },
+        function(err) {
+            console.log(err)
+        }
+    )
+}
+
 module.exports = {
     get_playlist_contents: get_playlist_contents,
     get_playlist_id: get_playlist_id,
+    get_playlists: get_playlists
 }
--- spotify-api.test.js
+++ spotify-api.test.js
@@ -7,7 +7,42 @@ it('learn to use promises', () => {
 
 it('handle exception', () => {
     expect.assertions(1)
+    api.get_playlists('21aamjqllbwgchskywbugsopa')
    return api.get_playlist_contents(false).catch(e => {
         expect(e).toEqual(Error("error"))
     })
 })
+
+const SpotifyWebApi = require('spotify-web-api-node')
+const spotify_api = new SpotifyWebApi({
+    clientid: '4561e903b7db4f5ea09b30842bfa3f67',
+    clientSecret: process.env.api_secret,
+    redirectUri: 'http://localhost'
+})
+/**
+ * handling limits
+ * while returned array is not emtpy
+ * see what happens when offest is crossed
+ * might need to do settimeout here as well, or wait for the response
+ * to finish before continuing offest and build
+ * array of tracks
+ * */
+spotify_api.setAccessToken(process.env.access_token)
+it('get user playlists', () => {
+    //expect.assertions(1)
+    // here need to modify offest and limit on each time
+    spotify_api.getPlaylistTracks('7kGIhqEo5HGYTlFFeIi1j7').then(
+        function(data) {
+            for (item of data.body.items) {
+                console.log(item.track.name)
+            }
+            expect(true).teEqual(true)
+        },
+        function(err) {
+            console.log(err)
+            expect(true).teEqual(true)
+        }
+    )
+})
+// http://localhost/?code=AQAuqiA-6seNum3lype4dYLlHSJ0TKXATBXsQuGH4W87i2fQuQ-IQCN587YaVZRmvzUWvk5W5nWlfAeDT2CmIBB50UI5RPmYefruZpbLSt0COPQWMe5tDk_NqpECtD1sP0AY-qdR2VgxCGfl81pYidN8Drc959PteSMzAXtJIZ6LhVSTp1s4ig
+// AQAuqiA-6seNum3lype4dYLlHSJ0TKXATBXsQuGH4W87i2fQuQ-IQCN587YaVZRmvzUWvk5W5nWlfAeDT2CmIBB50UI5RPmYefruZpbLSt0COPQWMe5tDk_NqpECtD1sP0AY-qdR2VgxCGfl81pYidN8Drc959PteSMzAXtJIZ6LhVSTp1s4i

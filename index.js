2e3c3cee04fbdc9d4bfff910ff2b6582f4a0c906
iarigby <marvinzem@gmail.com>
2019-09-29 09:41:56 +0400

    add second playlist, fix .env

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const min = require('./playlists-minimal')
 const liked_from_radio = '7kGIhqEo5HGYTlFFeIi1j7'
 const offline_playlist = '3jqWiGdctbwwRDAxRI9cCR'
 
+const offline_saved_playlist = '6fsReisctha13w0JfAYXYG'
+
 min.update_playlist(liked_from_radio, offline_playlist, 50)
+min.update_playlist('some_bs', offline_saved_playlist, 50, true)
--- set-up-api.js
+++ set-up-api.js
@@ -3,15 +3,12 @@ const auth = require('spotify-auth-node')
 
 const SpotifyWebApi = require('spotify-web-api-node')
 const spotify_api = new SpotifyWebApi({
-    clientid: '4561e903b7db4f5ea09b30842bfa3f67',
-    clientSecret: process.env.api_secret,
+    clientid: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     redirectUri: 'http://localhost:8888/callback'
 })
 
 auth.getAccessToken(token => 
     spotify_api.setAccessToken(token))
-
-// TODO find a better way to handle token refresh (maybe read/write from file)
-//  which makes deleting the untracked "database" file a bit stupid
-//  spotify_api.refreshAccessToken().then(data => console.log(data.body['access_token']))
+    
 module.exports = spotify_api

2f4b06c47883e6acf450fc2a63b25f29be07bfb9
iarigby <marvinzem@gmail.com>
2019-09-29 08:02:54 +0400

    use auth, remove dummy functions

--- set-up-api.js
+++ set-up-api.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
+const auth = require('spotify-auth-node')
+
 const SpotifyWebApi = require('spotify-web-api-node')
 const spotify_api = new SpotifyWebApi({
     clientid: '4561e903b7db4f5ea09b30842bfa3f67',
     clientSecret: process.env.api_secret,
     redirectUri: 'http://localhost:8888/callback'
 })
-spotify_api.setAccessToken(process.env.access_token)
+
+auth.getAccessToken(token => 
+    spotify_api.setAccessToken(token))
+
 // TODO find a better way to handle token refresh (maybe read/write from file)
 //  which makes deleting the untracked "database" file a bit stupid
 //  spotify_api.refreshAccessToken().then(data => console.log(data.body['access_token']))
--- spotify-api.js
+++ spotify-api.js
@@ -1,47 +1,39 @@
 const spotify_api = require('./set-up-api')
-function get_playlist_id(song_name) {
-    return new Promise()
+const auth = require('spotify-auth-node')
+
+function authorize() {
+    return auth.getAccessToken()
+        .then(token => spotify_api.setAccessToken(token))
 }
 
-function get_playlist_contents(playlist_id) {
-    return new Promise((resolve, reject) => {
-        if(playlist_id) {
-            resolve(["song1", "song2"])
-        } else {
-            reject(Error("error"))
+function get_last_tracks(playlist_id, limit = 100, saved_tracks) {
+    return authorize().then(() => {
+        if (saved_tracks) {
+            return spotify_api.getMySavedTracks({
+                limit: limit
+            })
         }
-    })
-}
-
-// mine is
-// 21aamjqllbwgchskywbugsopa
-// liked from radio playlist
-// 7kGIhqEo5HGYTlFFeIi1j7
-function get_playlists(user_id) {
-    spotify_api.getUserPlaylists(user_id).then(
-        function(data) {
-            console.log(data.body)
-        },
-        function(err) {
-            console.log(err)
-        }
-    )
-}
-
-function get_last_tracks(playlist_id, limit=100) {
-    return spotify_api.getPlaylistTracks(playlist_id, {
-        limit: limit
+        else
+            return spotify_api.getPlaylistTracks(playlist_id, {
+                limit: limit
+            })
     }).then(result => get_ids(result.body.items))
 }
 
 // TODO test these
 function add_songs(playlist_id, song_ids) {
-    return spotify_api.addTracksToPlaylist(playlist_id, song_ids.map(transform_id))
+    return authorize()
+        .then(() =>
+            spotify_api.addTracksToPlaylist(playlist_id, song_ids.map(transform_id))
+        )
 }
 
 function remove_songs(playlist_id, song_ids) {
     // this might need changing (without transform)
-    return spotify_api.removeTracksFromPlaylist(playlist_id, song_ids.map(transform_id))
+    return authorize()
+        .then(() =>
+            spotify_api.removeTracksFromPlaylist(playlist_id, song_ids.map(transform_id))
+        )
 }
 
 function transform_id(id) {
@@ -49,12 +41,9 @@ function transform_id(id) {
 }
 
 function get_ids(songs) {
-    return songs.map(song => {return song.track.id})
+    return songs.map(song => { return song.track.id })
 }
 module.exports = {
-    get_playlist_contents: get_playlist_contents,
-    get_playlist_id: get_playlist_id,
-    get_playlists: get_playlists,
     get_last_tracks: get_last_tracks,
     add_songs: add_songs,
     remove_songs: remove_songs

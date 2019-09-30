e973f862d1d6843834e4f752eb4c93f55c122861
iarigby <marvinzem@gmail.com>
2019-09-30 13:18:04 +0400

    setinterval, commented out to test  watchtower

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const offline_playlist = '3jqWiGdctbwwRDAxRI9cCR'
 
 const offline_saved_playlist = '6fsReisctha13w0JfAYXYG'
 
-min.update_playlist(liked_from_radio, offline_playlist, 50)
-min.update_playlist('some_bs', offline_saved_playlist, 50, true)
+function refresh() {
+    console.log('interval working')
+    // min.update_playlist(liked_from_radio, offline_playlist, 50)
+    // min.update_playlist('some_bs', offline_saved_playlist, 50, true)
+}
+
+setInterval(refresh, 60000)
